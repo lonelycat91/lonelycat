34918daef667857955ed544db40b2c6568b8b492
lonelycat91 <67738236+lonelycat91@users.noreply.github.com>
2022-07-20 15:29:57 +0700

    h

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,4 +22,36 @@ $(document).ready(function(){
             },
           ],
     });
+
+    $('.slide-blog').slick({
+      centerMode: true,
+      centerPadding: '250px',
+      // autoplay: true,
+      slidesToShow: 1,
+      infinite : true,
+      // autoplaySpeed: 2000,
+      arrow: true,
+      prevArrow:"<button type='button' class='slick-prev pull-left'><i class='bi bi-arrow-left-circle-fill'></i></button>",
+      nextArrow:"<button type='button' class='slick-next pull-right'><i class='bi bi-arrow-right-circle-fill'></i></button>",
+      responsive: [
+        {
+          breakpoint: 740,
+          settings: {
+            centerPadding: '0px',
+          },
+        },
+        {
+          breakpoint: 1200,
+          settings: {
+            centerPadding: '0px',
+          },
+        },
+      ],
+  });
+
+
+
+
+
+
   });
